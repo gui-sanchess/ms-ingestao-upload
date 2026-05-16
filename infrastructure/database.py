8cc0e880d81097b3486986dca1b1653d5d73fcba
Guilherme Sanches <10436672@mackenzista.com.br>
2026-05-16 16:16:37 -0300

    Bug corrigido

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -3,35 +3,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Captura as credenciais do banco
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "postgres")
-DB_SCHEMA = os.getenv("DB_SCHEMA", "upload")
+# Agora ele pega a URL inteira que colocamos lá no painel da Azure
+DATABASE_URL = os.getenv("DATABASE_URL")
+DB_SCHEMA = os.getenv("DB_SCHEMA", "upload") # Mantemos o schema do Upload
 
-# Monta a String de Conexão padrão do PostgreSQL
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-# O pulo do gato: Cria o motor do banco já apontando para o schema correto ('upload')
-# O connect_args com 'options' força o PostgreSQL a usar o nosso schema.
+# Cria o motor do banco apontando para o schema correto ('upload')
 engine = create_engine(
     DATABASE_URL,
     connect_args={"options": f"-csearch_path={DB_SCHEMA}"},
-    echo=True # Útil no desenvolvimento para ver os comandos SQL no terminal
+    echo=True
 )
 
-# Fábrica de Sessões: É o que o FastAPI vai usar para abrir e fechar conexões
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Classe Base que nossas tabelas vão herdar
 Base = declarative_base()
 
-# Dependência do FastAPI para injetar o banco de dados nas rotas
 def get_db():
     db = SessionLocal()
     try:
